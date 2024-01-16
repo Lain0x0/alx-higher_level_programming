e38006ce5fe36d1bab736be06d6a973e362cfcd5
Lain0x0 <lainyamato03@gmail.com>
2024-01-16 13:33:56 +0100

    Readme file for this project

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+if (number < 0):
+    print(f"the number {number :d} is negative")
+else:
+    if (number > 0):
+        print(f"the number {number :d} is positive")
+    else:
+        if (number == 0):
+            print(f"the number {number :d} is zero")
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,1 @@
+this is a readme for 0x01-python_if_else_loops_functions
