64fe1f4686b6709391e465b323d1759806386d3c
Lain0x0 <lainyamato03@gmail.com>
2024-01-16 15:06:28 +0100

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(1):
-    print("a b c d e f g h i j k l m n o p q r s t u v w x y z ", end="")
+    print(f"a b c d e f g h i j k l m n o p q r s t u v w x y z ", end="")
