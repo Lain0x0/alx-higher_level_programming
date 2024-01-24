88f2f5ce8d536df350e47ce95e7e02346d088e30
Lain0x0 <lainyamato03@gmail.com>
2024-01-24 22:43:53 +0100

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if (not is_instance(roman_string, str)):
+    if (not isinstance(roman_string, str)):
         return 0
     total = 0
     number = 0
