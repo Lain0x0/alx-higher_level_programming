0d30f58ec92f3e2fa39f5ad9db7c665f8fbd10a3
Lain0x0 <lainyamato03@gmail.com>
2024-02-06 21:34:02 +0100

    0x02-classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+class Square:
+    """ Define a square """
+    def __init__(self, size):
+        """ Constructor
+        Args:
+            size: length of a side the square
+        """
+        self.__size = size
