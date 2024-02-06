e80e78d760c9b64308bf423dba134620dcbcb719
Lain0x0 <lainyamato03@gmail.com>
2024-02-07 00:35:11 +0100

    0x102-classes

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,55 +1,54 @@
 #!/usr/bin/python3
-""" sqaure module """
 
 
 class Square:
-
-    """ Represent a square """
-    def __init__(self, size=0):
-        """ contructor.
-    Args:
-        size: length of side of square
-    """
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize Square instance with size and position."""
         self.size = size
+        self.position = position
 
     @property
     def size(self):
-        """Property for the length
-        Raise:
-            TypeError:If size is not int
-            ValueError:If size is less than
-        """
-        return (self.__size)
+        """Retrieve the size of the square."""
+        return self.__size
 
     @size.setter
     def size(self, value):
-        if (not isinstance(value, int)):
+        """Set the size of the square."""
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if (value < 0):
+        elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        """Retrieve the position of the square."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Set the position of the square."""
+        if (not isinstance(value, tuple) or len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
 
     def area(self):
-        """ Area of sqaure
-        Returns:
-            the size of square
-        """
-        return (self.__size ** 2)
+        """Return the area of the square."""
+        return self.__size ** 2
 
-    def __equal__(self, other):
-        return (self.area() == other.area())
+    def my_print(self):
+        """Print the square with the character #."""
+        if self.__size == 0:
+            print()
+            return
 
-    def __notequal__(self, other):
-        return (self.area() != other.area())
+        for _ in range(self.__position[1]):
+            print()
 
-    def __greaterthan__(self, other):
-        return (self.area() > other.area())
-
-    def __greaterequal__(self, other):
-        return (self.area() <= other.area())
-
-    def __lessthan__(self, other):
-        return (self.area() < other.area())
-
-    def __lessequal__(self, other):
-        return (self.area() <= other.area())
+        for _ in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
