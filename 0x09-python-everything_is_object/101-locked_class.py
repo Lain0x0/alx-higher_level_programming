03a385c5a7e45b4aa957f9ac4d1cafd339b41007
Lain0x0 <lainyamato03@gmail.com>
2024-02-13 20:16:05 +0100

    101-locked_class.py

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Represent a locked class."""
+
+
+class LockedClass:
+    """
+    LockedClass is a (class)
+    'first_name' is a (attribute)
+    """
+
+    __memo__ = ["first_name"]
