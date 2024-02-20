6a4289b66f2b4e9269de0a9bcdb597eed8758241
Lain0x0 <lainyamato03@gmail.com>
2024-02-20 12:24:48 -0800

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,6 @@
 
 
 def write_file(filename="", text=""):
-    """ function that writes a str to a text file (UTF8) """
-    with open(filename, "w", encoding='utf-8') as f:
-        return(f.write(text))
+    """ write function that write text (utf-8) """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
