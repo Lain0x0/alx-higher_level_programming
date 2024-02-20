e7240ea5995fd86c48661f6b49d79772fc12306c
Lain0x0 <lainyamato03@gmail.com>
2024-02-20 13:25:55 -0800

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Defines save_to_jsonn function """
+""" Defines save_to_json function """
 
 
 import json
@@ -7,5 +7,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """ write a script that add all arguments """
-    with open(filename, "w", encoding="utf-8")as f:
-        json.dump(my_obj, f)
+    with open(filename, 'w', encoding="utf-8")as f:
+        json.dumps(my_obj, f)
