927f6e00d32a765e6495b058509259ae0e859dcc
Lain0x0 <lainyamato03@gmail.com>
2024-02-26 21:33:39 +0100

    square.exe

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-""" method for square class. """
+""" Method for square class. """
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ Class Square that inherist from Rectangle """
+    """ Class Square that inherits from Rectangle """
 
     def __init__(self, size, x=0, y=0, id=None):
         """ Constructor """
-        self.size = size
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ Return str info about square """
+        return '[{}] ({}) {}/{} - {}'.\
+            format(type(self).__name__, self.id, self.x, self.y, self.width)
