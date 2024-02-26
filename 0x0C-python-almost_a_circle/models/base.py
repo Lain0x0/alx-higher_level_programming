9bf805568419e4c2620322651f2ff53569eed6d4
Lain0x0 <lainyamato03@gmail.com>
2024-02-26 17:53:39 +0100

    base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-""" Module for base class """
+""" Module for Base class. """
 
 
 class Base:
-    """ Definning base as OOP """
+    """ Defines base as OOP """
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Constructor"""
-        if (id is None):
+        """ Constructor """
+        if (id is not None):
             self.id = id
         else:
             Base.__nb_objects += 1
