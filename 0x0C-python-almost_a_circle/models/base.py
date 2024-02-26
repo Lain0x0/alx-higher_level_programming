a53abed1dc71818f87ac251139b01130657172f0
Lain0x0 <lainyamato03@gmail.com>
2024-02-27 00:40:50 +0100

    0x0Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,3 +25,13 @@ class Base:
         if (list_dictionaries is None or list_dictionaries == []):
             return ("[]")
         return (json.dumps(list_dictionaries))
+
+    def save_to_file(cls, list_objs):
+        """Write JSON of a list of obj """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as jsonfile:
+            if (list_objs is None):
+                jsonfile.write("[]")
+            else:
+                list_dicts = [o.to_dictionary() for o in list_objs]
+                jsonfile.write(Base.to_json_string(list_dicts))
