79fb5fa806f377feea28395d5849a51ebc531fdf
Lain0x0 <lainyamato03@gmail.com>
2024-03-26 22:07:37 +0000

    DB-05

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,3 @@
 -- Creating your first table in SQL:
-CREATE TABLE IF NOT EXISTS first_table(
- id, INT
- name, VARCHAR(256)
-);
+CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256));
 
