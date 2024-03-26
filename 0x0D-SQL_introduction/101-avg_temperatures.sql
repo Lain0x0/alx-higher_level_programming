1fd738d0facff22e84e8c6e5196990ec12d74bf2
Lain0x0 <lainyamato03@gmail.com>
2024-03-26 23:35:39 +0000

    DB-19

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- Importing DATABASE by using SQL:
-SELECT city, AVG(value) as avg_tmp
+SELECT city, AVG(value) as avg_temp
 FROM temperatures
 GROUP BY city
-ORDER BY avg_tmp DESC;
+ORDER BY avg_temp DESC;
