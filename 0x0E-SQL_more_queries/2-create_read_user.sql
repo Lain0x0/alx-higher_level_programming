a021d3c4ec0498c3d607329e411ef88217bc877b
Lain0x0 <lainyamato03@gmail.com>
2024-03-27 15:46:13 +0000

    SQL_0x3-More_Queries

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- Creating a new DATABASE for a user with SQL:
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS user_0d_2;
-GRANTS ALL PRIVILEGS ON hbtn_0d_2 TO "user_0d_2"@"localhost" IDENTIFIED BY "user_0d_2_pwd";
+CREATE USER IF NOT EXISTS "user_0d_2"@"localhost" IDENTIFIED BY "user_0d_2_pwd";
+GRANTS ALL PRIVILEGS ON "hbtn_0d_2".* TO "user_0d_2"@"localhost";
