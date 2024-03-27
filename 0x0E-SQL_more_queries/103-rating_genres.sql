9d8745e21bb37779842e2d7db85dbd7a96165623
Lain0x0 <lainyamato03@gmail.com>
2024-03-27 18:14:32 +0000

    SQL_0x21-More_Queries

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,9 +1,8 @@
 -- Importing database to dump from DB 2.0 with SQL:
--- Importing database to dump from DB 2.0 with SQL
-SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rat
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-JOIN tv_show_rat ON tv_show_rat.show_id = tv_shows.id
+JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
 GROUP BY tv_genres.name
-ORDER BY rat DESC;
+ORDER BY rating DESC;
