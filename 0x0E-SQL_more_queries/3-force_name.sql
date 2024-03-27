0d019a2363b6242c6aab0fe3a2a2d1c0954d1199
Lain0x0 <lainyamato03@gmail.com>
2024-03-27 16:10:51 +0000

    SQL_0x4-More_Queries

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,2 @@
 -- Creating a new TABLE for new DATABASE with SQL:
-CREATE TABLE IF NOT EXISTS "force_name"("id" INT, "name" VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name("id" INT, "name" VARCHAR(256) NOT NULL);
