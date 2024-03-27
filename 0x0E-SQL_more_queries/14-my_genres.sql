da91be8bb99f8ad754b18681a422f30674312848
Lain0x0 <lainyamato03@gmail.com>
2024-03-27 17:29:46 +0000

    SQL_0x15-More_Queries

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,10 @@
+-- Import the database dump from DATABASE with SQL:
+SELECT gen.`name`
+  FROM `tv_genres` AS gen
+       INNER JOIN `tv_show_genres` AS ser
+       ON gen.`id` = s.`genre_id`
+
+       INNER JOIN `tv_shows` AS tv_s
+       ON tv_s.`id` = ser.`show_id`
+       WHERE tv_S.`title` = "Dexter"
+ ORDER BY gen.`name`;
