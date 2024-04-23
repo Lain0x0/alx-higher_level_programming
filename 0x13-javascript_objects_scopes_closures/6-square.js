83966f4714fc279a47f7b0a83776e0fa39c24f42
Lain0x0 <lainyamato03@gmail.com>
2024-04-23 17:26:12 +0100

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,21 +1,24 @@
 #!/usr/bin/node
 
 const Rectangle = require('./5-square.js');
+
 class Square extends Rectangle {
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
     }
-    const i = 0;
-    while (i < this.width) {
+    let i = 0;
+    while (i < this.height) {
       let j = 0;
       let char = '';
-      while (j < this.height) {
-        char += 'X';
+      while (j < this.width) {
+        char += c;
         j++;
       }
       console.log(char);
+      i++;
     }
   }
 }
+
 module.exports = Square;
