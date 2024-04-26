b296f22a074b04a8d6b9b1888dd4da47f31088e9
Lain0x0 <lainyamato03@gmail.com>
2024-04-26 10:53:20 -0700

    0x0 orm

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
-""" Program that gets all states sorted
-in ascending ordered by states id"""
+"""Listing all states with name start with N% """
 
 if __name__ == "__main__":
 
@@ -12,15 +11,14 @@ if __name__ == "__main__":
             host="localhost",
             port=3306,
             user=sys.argv[1],
-            passwd=sys.argv[2],
+            passwrd=sys.argv[2],
             db=sys.argv[3]
             )
-
     curs = db.cursor()
-    curs.execute("SELECT * FROM states ORDER BY states.id ASC")
-    rows = curs.fetchall()
+    curs.execute("""
+            SELECT * FROM states WHERE name
+            LIKE BINARY '%N' ORDER BY states.id ASC;""")
 
+    rows = curs.fetchall()
     for row in rows:
         print(row)
-    curs.close()
-    db.close()
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
-""" Program that gets all states sorted
-in ascending ordered by states id"""
+""" """
 
 if __name__ == "__main__":
 
@@ -12,15 +11,14 @@ if __name__ == "__main__":
             host="localhost",
             port=3306,
             user=sys.argv[1],
-            passwd=sys.argv[2],
+            passwrd=sys.argv[2],
             db=sys.argv[3]
             )
 
     curs = db.cursor()
-    curs.execute("SELECT * FROM states ORDER BY states.id ASC")
-    rows = curs.fetchall()
+    curs.execute(" SELECT * FROM states WHERE name LIKE BINARY '{}'\
+                    ORDER BY states.id ASC".format(sys.argv[4]))
 
+    rows = curs.fetchall()
     for row in rows:
         print(row)
-    curs.close()
-    db.close()
