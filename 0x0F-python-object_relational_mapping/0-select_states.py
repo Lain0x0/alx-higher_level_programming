e3a7d4a6486a87fd32b6125ccb4c1f842dc15dea
Lain0x0 <lainyamato03@gmail.com>
2024-04-26 10:31:01 -0700

    0x0 orm

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+""" Program that gets all states sorted
+in ascending ordered by states id"""
+
+if __name__ == "__main__":
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+            )
+
+    curs = db.cursor()
+    curs.execute("SELECT * FROM states ORDER BY states.id ASC")
+    rows = curs.fetchall()
+
+    for row in rows:
+        print(row)
