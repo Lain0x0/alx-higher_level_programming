3fa52bc3bd80cb6a848a1bd63ab7abe1473741f9
Lain0x0 <lainyamato03@gmail.com>
2024-04-27 08:43:15 -0700

    00

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,20 @@
-#!/usr/bin/env python3
-""" Program that gets all states sorted
-in ascending ordered by states id """
+#!/usr/bin/python3
+"""Lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
 
-    db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3]
-            )
+    c = db.cursor()
 
-    curs = db.cursor()
-    curs.execute("SELECT * FROM states ORDER BY id ASC")
+    c.execute("SELECT * FROM states ORDER BY id ASC")
+    [print(x) for x in c.fetchall()]
 
-    for row in curs.fetchall():
-        print(row)
-
-    curs.close()
+    c.close()
     db.close()
