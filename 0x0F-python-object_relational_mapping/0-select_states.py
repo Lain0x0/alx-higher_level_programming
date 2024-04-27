e7fbfc88fc8c26cc88da5a09346da1d2ccf931e7
Lain0x0 <lainyamato03@gmail.com>
2024-04-27 09:46:44 -0700

    0x0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
-
 """ Program that gets all states sorted
 in ascending ordered by states id"""
+import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-
-    import MySQLdb
-    import sys
-
     db = MySQLdb.connect(host="localhost",
                          port=3306,
                          user=sys.argv[1],
