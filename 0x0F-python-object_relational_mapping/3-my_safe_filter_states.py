7a06cb9d601387474c1d4c0f44f9d3d6a86561a3
Lain0x0 <lainyamato03@gmail.com>
2024-04-27 14:30:58 -0700

    0x03--ORM

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
                          db=sys.argv[3])
 
     curs = db.cursor()
-    curs.execute("SELECT * FROM states WHERE name LIKE %s", (sys.argv[4]))
+    curs.execute("SELECT * FROM states WHERE name LIKE %s", (sys.argv[4], ))
     rows = curs.fetchall()
     for row in rows:
         print(row)
