7ec118a039e58970ccfe68f83f6f123c4d4b2cbe
Lain0x0 <lainyamato03@gmail.com>
2024-04-27 10:09:02 -0700

    0x1--ORM

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,16 +2,15 @@
 
 """Listing all states with name start with N% """
 
+import MySQLdb
+import sys
+
 if __name__ == "__main__":
-
-    import MySQLdb
-    import sys
-
     db = MySQLdb.connect(
             host="localhost",
             port=3306,
             user=sys.argv[1],
-            passwrd=sys.argv[2],
+            passwd=sys.argv[2],
             db=sys.argv[3]
             )
     curs = db.cursor()
