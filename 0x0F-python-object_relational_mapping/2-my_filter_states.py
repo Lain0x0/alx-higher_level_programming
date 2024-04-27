6063148024fd455b368974c09f2370c1dc6a040d
Lain0x0 <lainyamato03@gmail.com>
2024-04-27 10:36:29 -0700

    0x02--ORM

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+""" """
+
+if __name__ == "__main__":
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+            )
+
+    curs = db.cursor()
+    curs.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
+                    ORDER BY id ASC".format(sys.argv[4]))
+
+    rows = curs.fetchall()
+    for row in rows:
+        print(row)
+
+    curs.close()
+    db.close()
