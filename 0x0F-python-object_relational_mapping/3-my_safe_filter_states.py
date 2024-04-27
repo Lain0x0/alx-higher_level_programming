f1034fb5b43fb4281a0323813f2726450a580399
Lain0x0 <lainyamato03@gmail.com>
2024-04-27 12:41:37 -0700

    0x03--ORM

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    curs = db.cursor()
+    curs.execute("SELECT * FROM `states`")
+
+    rows = curs.fetchall()
+
+    for row in rows:
+        print(row)
+
+    curs.close()
+    db.close()
