af3bfb56ac2e08e5d8ea5547f0c0f2a6fd05aad1
Lain0x0 <lainyamato03@gmail.com>
2024-04-27 16:05:30 -0700

    0x08-ORM

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" This a comment """
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    c = session.query(State).first()
+    print("Nothing" if c is None else f"{c.id}: {c.name}")
