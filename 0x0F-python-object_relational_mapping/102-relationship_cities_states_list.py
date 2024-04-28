e3b3afc8c139b1d2ae18999fd2ca97a93eed8834
Lain0x0 <lainyamato03@gmail.com>
2024-04-27 17:41:07 -0700

    0x102--ORM

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""prints the State object with the name passed as argument from dtb"""
+"""prints the State obj"""
 import sys
 from relationship_state import Base, State
 from relationship_city import City
