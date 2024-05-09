6bf380e4ae5ab1032e97e32ae5725c2db58d0753
Lain0x0 <lainyamato03@gmail.com>
2024-05-09 18:58:56 +0100

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Writing a bash scripts that take and URL sends a REQUEST to that URL
 # And showing the size of of the body of the response
-curl -s "$1" | wc -c
+curl -sI "$1" | awk '/Content-Length/ {print $2}'
 
