52b3ea64b60f306e4b8144376ef55748eef5e23b
Lain0x0 <lainyamato03@gmail.com>
2024-06-04 14:29:31 +0100

    0x7-completedtask.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+if (!(process.argv[2])) {
+  console.error('Please provide API URL: https://jsonplaceholder.typicode.com/todos');
+  process.exit(1);
+}
+
+request(process.argv[2], (err, response, body) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  const tasks = JSON.parse(body);
+  const completedTasks = {};
+  for (const task of tasks) {
+    if (task.completed) {
+      if (completedTasks[task.userId]) {
+        completedTasks[task.userId]++;
+      } else {
+        completedTasks[task.userId] = 1;
+      }
+    }
+  }
+  console.log(completedTasks);
+});
