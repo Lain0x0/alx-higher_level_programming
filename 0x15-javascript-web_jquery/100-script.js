381e708cb908c76916443be4d91e9d86ba4fe990
Lain0x0 <lainyamato03@gmail.com>
2024-06-12 23:30:33 +0100

    100-script.js

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,5 @@
+// Script that update an HTML Tag #11
+document.addEventListener('DOMContentLoaded', () => {
+  const header = document.querySelector('header');
+  header.style.color = '#FF0000';
+});
